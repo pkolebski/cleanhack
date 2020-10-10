85cfb9f1b23af19f7e495e54fb4fa5d22ca5ca52
Piotr Kolebski <piotrkolebski@gmail.com>
2020-10-10 17:31:52 +0200

    models: Improve stance detection

--- cleanhack/models/stance.py
+++ cleanhack/models/stance.py
@@ -4,7 +4,13 @@ from cleanhack.models.base import Model
 
 
 class Stance(Model):
-    def get_prediction(self, tweet: pd.DataFrame):
+    def predict(self, tweets: pd.DataFrame, *args, **kwargs):
+        outputs = []
+        for tweet in tweets.iterrows():
+            outputs.append(self.get_prediction(tweet[1]))
+        return outputs
+
+    def get_prediction(self, tweet: pd.Series, *args):
         good_energy = tweet.topic_clean_energy or tweet.topic_photovoltaics or \
                       tweet.topic_gas or tweet.topic_nuclear
         if tweet.sentiment == 'positive' and good_energy and \
