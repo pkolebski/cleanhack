45613c76c4b715116867947434073fd9a0883316
Jakub Binkowski <238382@student.pwr.edu.pl>
2020-10-10 13:13:42 +0200

    Wrap ner with class

--- cleanhack/models/ner.py
+++ cleanhack/models/ner.py
@@ -1,24 +1,29 @@
+from typing import List
+
 import spacy
 
-model_name = 'en_core_web_lg'
+from cleanhack.models.base import Model
+
+DEFAULT_MODEL = 'en_core_web_lg'
 
 
-def get_entities_from_text(tweets, nlp=None):
-    if nlp is None:
-        nlp = spacy.load(model_name, disable=['tagger', 'parser'])
+class NamedEntityRecognizer(Model):
+    def __init__(self):
+        self.nlp = spacy.load(DEFAULT_MODEL, disable=['tagger', 'parser'])
 
-    extracted = {'locations': [], 'organisations': []}
-    for tweet_doc in nlp.pipe(tweets):
-        extracted['locations'].append(_extract_ent(tweet_doc, 'GPE'))
-        extracted['organisations'].append(_extract_ent(tweet_doc, 'LOC'))
-    return extracted
+    def get_prediction(self, tweets: List[str]):
+        extracted = {'locations': [], 'organisations': []}
+        for tweet_doc in self.nlp.pipe(tweets):
+            extracted['locations'].append(self._extract_ent(tweet_doc, 'GPE'))
+            extracted['organisations'].append(self._extract_ent(tweet_doc, 'LOC'))
+        return extracted
 
-
-def _extract_ent(doc, target_label):
-    return [ent.text for ent in doc.ents if ent.label_ == target_label]
+    def _extract_ent(self, doc, target_label):
+        return [ent.text for ent in doc.ents if ent.label_ == target_label]
 
 
 if __name__ == '__main__':
     example_tweets = ["There was an event in New York. Fortum Clean Energy provides energy."]
-    extracted_metadata = get_entities_from_text(example_tweets)
+    model = NamedEntityRecognizer()
+    extracted_metadata = model.get_prediction(example_tweets)
     print(extracted_metadata)
