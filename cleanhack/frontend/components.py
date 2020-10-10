a14f0ce31f0d8fd84f4a33670fbb216232bf5ab9
Mateusz Wójcik <mateuszwojcik170@gmail.com>
2020-10-10 18:15:30 +0200

    feat: add magnus opum

--- cleanhack/frontend/components.py
+++ cleanhack/frontend/components.py
@@ -1,26 +1,62 @@
+import glob
 import time
 import numpy as np
 import streamlit as st
+from PIL import Image
+import matplotlib.pyplot as plt
+
+from component_template.template.my_component import my_component
+
+FOLDER_FOR = '/home/mateusz/PycharmProjects/cleanhack/cleanhack/frontend/images/for/'
+FOLDER_AGAINST = '/home/mateusz/PycharmProjects/cleanhack/cleanhack/frontend/images/against/'
+FOLDER_NEUTRAL = '/home/mateusz/PycharmProjects/cleanhack/cleanhack/frontend/images/neutral/'
+FOLDER_ASSETS = '/home/mateusz/PycharmProjects/cleanhack/cleanhack/frontend/images/assets/'
+
+
+def get_technologies_plot(data_path):
+    images = glob.glob(f"{data_path}*.png")
+    fig, axs = plt.subplots(nrows=2, ncols=4, figsize=(30, 20), dpi=100)
+    for index, img in enumerate(images):
+        print(index // 4, index % 4)
+        image = Image.open(img)
+        axs[index // 4, index % 4].axis('off')
+        axs[index // 4, index % 4].imshow(image)
+
+    return fig
+
+
+def load_tweets(folder):
+    images = glob.glob(f"{folder}*.png")
+    for index, image in enumerate(images):
+        image = Image.open(image)
+        st.image(image)
 
 
 def home():
-    progress_bar = st.sidebar.progress(0)
-    last_rows = np.random.randn(1, 1)
-    chart = st.line_chart(last_rows)
-
-    for i in range(1, 101):
-        new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
-        chart.add_rows(new_rows)
-        progress_bar.progress(i)
-        last_rows = new_rows
-        time.sleep(0.05)
-
-    progress_bar.empty()
+    st.title("Smart&Green.ai")
+    st.text('A social media monitoring system.'
+            ' that  about'
+            ' environmental issues and ')
+    st.text('Provides information on the emotional nature of tweets')
+    st.text('Perform data analysis and divide users into thematic groups')
+    fig = get_technologies_plot(FOLDER_ASSETS)
+    st.pyplot(fig)
 
 
 def analysis():
-    return None
+    st.title("Data analysis")
 
 
-def user_stance():
-    return None
+def tweets_for():
+    st.title("Tweets for")
+    load_tweets(FOLDER_FOR)
+
+
+def tweets_against():
+    st.title("Tweets against")
+    load_tweets(FOLDER_AGAINST)
+
+
+def tweets_neutral():
+    st.title("Tweets neutral")
+    load_tweets(FOLDER_NEUTRAL)
--- cleanhack/frontend/run.py
+++ cleanhack/frontend/run.py
@@ -1,16 +1,17 @@
 import streamlit as st
 import numpy as np
 
-from components import home, analysis, user_stance
+from components import home, analysis, tweets_for, tweets_against, tweets_neutral
 from data_loading import load_data
 
 # Settings
 
 COMPONENTS = {
-    '-': home,
     'Home': home,
     'Analysis': analysis,
-    'User stance': user_stance
+    'Users For': tweets_for,
+    'Users Against': tweets_against,
+    'Users Neutral': tweets_neutral
 }
 
 # Data loading
@@ -19,24 +20,13 @@ df = load_data()
 
 # Layout start
 
-st.title("Clean energy")
-
 # Menu start
 
-mode = st.sidebar.selectbox("", list(COMPONENTS.keys()))
+mode = st.sidebar.selectbox("Home", list(COMPONENTS.keys()))
 
 # Menu end
 
-# components_to_pass = {
-#     '-': {'last_rows': last_rows, 'progress_bar': progress_bar, 'chart': chart},
-#     'Home': {'last_rows': last_rows, 'progress_bar': progress_bar, 'chart': chart},
-#     'Analysis': {},
-#     'User stance': {}
-# }
-
 # Render components
 COMPONENTS[mode]()
-
-st.button("Refresh")
 
 # Layout end
